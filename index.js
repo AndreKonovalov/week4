0c3ec535248d767937d8d83bf5aed333c2befa29
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-15 16:49:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Server } = require('http');
+const finalhandler = require(finalhandler);
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+  'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
+};
+
+const s = Server((req, res) => {
+  console.log('url: ' + req.url);
+  console.log('\nmethod: ' + req.method + '\n');
+  if(req.url === '/result4/') {
+    let jobj = {
+      message: "id319887435",
+      'x-result': req.headers['x-test'];
+    };
+	
+    let buffer = '';
+    req.on('data', d => (buffer += d));
+    req.on('end', () => {
+      jobj['x-body'] = buffer;
+      let strj =  jobj.stringify();
+      console.log('strj = ' + strj + '\n');
+      res.writeHead(200, { 'Contente-Type': 'application/json; charset=utf-8', ...CORS });
+      res.end(strj);
+    });
+  } else {
+    return finalhandler(req, res);
+  }
+});
+s.listen(process.env.PORT, console.log('port = ' + process.env.PORT + ' ' + s.address().port) + '\n');
+
