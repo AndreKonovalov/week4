bafb779be7c448ffd28ef4d675c212d33c2e31cb
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-16 13:19:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ const s = Server((req, res) => {
     return finalhandler(req, res);
   }
 });
+
+s.listen(4321);
+console.log('port = ' + process.env.PORT + '\n')
 console.log('adr ' + s.address() + '\n');
 console.log('prt ' + s.address().port + '\n');
-s.listen(process.env.PORT, console.log('port = ' + process.env.PORT + '\n'));
